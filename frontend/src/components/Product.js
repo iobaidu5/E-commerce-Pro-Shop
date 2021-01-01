46d8239e99f276145480bb183046bff311dbdad0
iobaidu5 <i.obaidu5@gmail.com>
2020-12-31 23:33:28 -0800

    Backend

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import{ Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
+import axios from 'axios';
 
-const Product = ({product}) => {
+const Product = ({match}) => {
+
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = axios.get(`/api/products/${match.params.id}`);
+            setProduct(data);
+        }
+          fetchProduct();
+    }, []);
+    
     return (
         <Card className='my-3 p-3 rounded'>
             <Link to={`/product/${product._id}`}>
