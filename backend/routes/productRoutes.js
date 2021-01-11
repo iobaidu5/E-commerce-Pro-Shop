7cf353561e3fccc568aa8885300446bb5e4ab84d
iobaidu5 <i.obaidu5@gmail.com>
2021-01-11 23:30:07 +0500

    Error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,24 +8,23 @@ import Product from '../models/productModel.js';
 // @routes  GEt all products
 // @access Public 
 
-
 router.get('/', asyncHandler(async (req,res) => {
     const products = await Product.find({});
 
     res.json(products);
 }));
 
-// @desc Fetch single products
-// @routes  GEt single products
+// @desc Fetch single product
+// @routes  GEt single product/:id
 // @access Public 
-
 router.get('/:id', asyncHandler(async (req,res) => {
    const product = await Product.findById(req.params.id);
 
    if(product) {
        res.json(product)
    } else {
-       res.status(404).json({ message: 'Product Not Found'});
+       res.status(404)
+       throw new Error("Product Not Found")
    }
    res.json(product);
 }));
