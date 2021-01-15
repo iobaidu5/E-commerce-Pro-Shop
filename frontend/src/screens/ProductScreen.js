ceaf38cbfe07f90da1093e80fae2868bdd1b21d4
iobaidu5 <i.obaidu5@gmail.com>
2021-01-15 23:32:40 +0500

    Redux Completed

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,92 +1,93 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import Rating from '../components/Rating';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import Message from '../components/message';
+import Loader from '../components/loader';
+import { listProductsDetails } from "../actions/productActions";
+
 const ProductScreen = ({ match }) => {
-    const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+  useEffect(() => {
+    dispatch(listProductsDetails(match.params.id));
+  }, [dispatch, match]);
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`);
-            setProduct(data);
-        }
-
-        fetchProduct();
-    },[match]);
-    return (
-        <>
-        <Link className='btn btn-light my-3' to='/'>
-            Go Back
-        </Link>
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <Row>
-            <Col md={6}>
-                <Image src={product.image} alt={product.name} fluid />
-            </Col>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
 
-            <Col md={3}>
-                <ListGroup variant='flush'>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
 
-                   <ListGroup.Item>
-                       <h3>{product.name}</h3>
-                   </ListGroup.Item> 
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
 
-                   <ListGroup.Item>
-                       <Rating
-                          value={product.rating}
-                          text={`${product.numReviews} reviews`}
-                        />
-                   </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
 
-                   <ListGroup.Item>
-                       Price: ${product.price}
-                   </ListGroup.Item>
+              <ListGroup.Item>
+                Description: ${product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
 
-                   <ListGroup.Item>
-                       Description: ${product.description}
-                   </ListGroup.Item> 
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
 
-                </ListGroup>
-            </Col>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
 
-            <Col md={3}>
-                <Card>
-                    <ListGroup variant='flush'>
-
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Price:</Col>
-                                <Col>
-                                  <strong>${product.price}</strong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Status:</Col>
-                                <Col>
-                                  {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Button 
-                            className='btn-block' 
-                            type='button'
-                            disabled={product.countInStock === 0} >
-                                Add To Cart
-                            </Button>
-                        </ListGroup.Item>
-                        
-                    </ListGroup>
-                </Card>
-            </Col>
+                <ListGroup.Item>
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
         </Row>
-            
-        </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productListReducer.js';
+import { productListReducer, productDetailsReducer } from './reducers/productListReducer.js';
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 });
 
 const initiaState = {};
